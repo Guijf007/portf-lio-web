74fafd5a3edac1cdc7946aa4a93328de5269f964
Guijf007 <124809960+Guijf007@users.noreply.github.com>
2023-10-31 21:19:33 -0300

    Add files via upload

---
+++ mim.js
@@ -0,0 +1,9 @@
+function apertado(){
+    alert("Você apertou o botão!!");
+}
+function mudar(){
+    document.body.style.backgroundImage="url('https://wallpapers.com/images/hd/aesthetic-sky-doodles-laptop-xhzkj8xwq5p2nftd.jpg')"
+}
+function voltar(){
+    document.body.style.backgroundImage="url('https://c4.wallpaperflare.com/wallpaper/87/851/622/laptop-backgrounds-nature-images-1920x1200-wallpaper-preview.jpg')";
+}
